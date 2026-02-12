b94a8e0db2c38866b490343d965610efcf19e378
snakdrag <treelin890@gmail.com>
2026-02-12 19:24:21 +0800

    修正背景更新

--- core/task.py
+++ core/task.py
@@ -15,20 +15,27 @@ class Task(Engine):
             try:await self._process_resolved(task)
             except Exception as e:print(e)
     async def _process_resolved(self,task):
-        task = await self.db.find_one(_const.BATTLE,ID=task[_const.ID])
+        task_id = task[_const.ID]
+        task = await self.db.find_one(_const.BATTLE,ID=task_id)
+        if not task: return
         guild_id = task.get(_const.GUILD_ID)
         group = task.get(_const.GROUP)
         attacker_id = task.get(_const.USER_ID)
         attacker_name = task.get(_const.NAME)
+        channel_id = task.get(_const.CHANNEL_ID)
+        channel = self.bot.get_channel(channel_id)
+        if not channel:
+            try:channel = await self.bot.fetch_channel(channel_id)
+            except discord.NotFound:return print(f"Channel {channel_id} not found.")
         attacker = await self.db.find_one(
             _const.PLAYER,user_id=attacker_id,
             name=attacker_name,guild_id=guild_id,group=group)
+        target_data = task[_const.TARGET]
         target = await self.db.find_one(
             _const.PLAYER,group=group,guild_id=guild_id,
-            user_id=task[_const.TARGET][_const.USER_ID],name=task[_const.TARGET][_const.NAME])
+            user_id=target_data[_const.USER_ID],name=target_data[_const.NAME])
         return_text = await Command(self.db,task.get(_const.CMD),attacker,[target]).execute()
-        await self.db.bulk_write(DeleteOne(_const.BATTLE,ID=task[_const.ID]))
-        channel = self.bot.get_channel(task.get(_const.CHANNEL_ID))
+        await self.db.bulk_write(DeleteOne(_const.BATTLE,ID=task_id))
         if task.get(_const.COST_TURN):
             turn = await self.next_turn(
                 group=group,user_id=attacker_id,
