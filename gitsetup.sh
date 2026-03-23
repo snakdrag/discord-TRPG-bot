de2c5e41458725946d5acd2636962ee3223376e8
SnakDrag <treelin890@gmail.com>
2026-03-23 08:14:38 +0800

    Delete gitsetup.sh

--- gitsetup.sh
+++
@@ -1,3 +0,0 @@
-git config --global user.email "treelin890@gmail.com"                                                                            ⌂ 11:24
-git config --global user.name "snakdrag"
-git config pull.rebase true
